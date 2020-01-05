5d3fc6a8e0513dc14264b4955ab5ee3355167a93
Ahmed Hussein <wolktheline@yahoo.com>
2020-01-05 22:13:42 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog 
+  def initialize(name,breed)
+    @name=name
+    @breed=breed
+  end 
+  def name ,breed 
+    @name=name
+    @breed=breed
+  end 
+  def name , breed = (new_name,_new_breed)
+    @name=new_name
+    @breed=new_bread
+  end 
+end 
